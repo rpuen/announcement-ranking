fa55d6cb9eb509682f56c6836c20dde6a5ae91bd
Roberto Puente <rpi@softtelecom.eu>
2019-01-12 18:26:27 +0100

    initial commit

---
+++ announcement-ranking/src/main/java/com/juniordev/idealista/ranking/announcementranking/AnnouncementRankingApplication.java
@@ -0,0 +1,14 @@
+package com.juniordev.idealista.ranking.announcementranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnouncementRankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnouncementRankingApplication.class, args);
+	}
+
+}
+
